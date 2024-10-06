86ccf2c09e491ebb89c1c97ea437752fb62b468d
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-06 22:56:39 +0600

    added card details

--- scripts/video.js
+++ scripts/video.js
@@ -14,12 +14,66 @@ const loadCatagories = () => {
     
 }
 
+//Create load videos function 2 
+const loadVideos = () => {
 
-//Create DisplayCategories function 2
+    //fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error))
+    
+}
+
+
+
+
+//Create display videos function 3
+const displayVideos = (videos) => {
+//    console.log(videos);
+const videoContainer = document.getElementById('videos')
+
+videos.forEach(video => {
+// console.log(video);
+const card = document.createElement('div')
+card.classList = "card card-compact"
+card.innerHTML = `
+ <figure class ="h-[200px]" >
+    <img
+      class ="h-full w-full object-cover"
+      src=${video.thumbnail} />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+
+   <div>
+     <img class ="rounded-full object-cover w-10 h-10" src=${video.authors[0].profile_picture} alt="" />
+   </div>
+
+   <div>
+     <h2 class ="font-bold">${video.title}</h2>
+     <div class ="flex items-center gap-2" >
+     <p class ="text-gray-400">${video.authors[0].profile_name}</p>
+     <img class ="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="" />
+     </div>
+     <p></p>
+   </div>
+
+  </div>
+`
+
+//add card to video container 
+videoContainer.append(card)
+
+})
+   
+}
+
+
+
+//Create DisplayCategories function 4
 const displayCategories = (categories) => {
 //  console.log(categories);
 const categoriesContainer = document.getElementById('categories')
-
 
 categories.forEach( (item) => {
     console.log(item);
@@ -38,3 +92,4 @@ categories.forEach( (item) => {
 
 
 loadCatagories()
+loadVideos()

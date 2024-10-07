97d5f7511f88a517213748c0aa6f53a50af5c942
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-07 13:58:22 +0600

    added search functionality and summery.js

---
+++ scripts/summery.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Load and display data initially
+ * 2. On click load and display data
+ * 3. Complex object accessing
+ * 4. Toggle active class functionality
+ * 5. Search functionality
+ * 6. Load and display single data
+ * 7. display data in a modal
+ * 8. Conditional rendering if array don't have any data
+ * 9. Conditional Rendering: if a value empty
+ * 10.Conditional Rendering: If a value length is zero
+ * 11. Load and display single category data
+ */
--- scripts/video.js
+++ scripts/video.js
@@ -37,10 +37,10 @@ const loadCatagories = () => {
 }
 
 //Create load videos function 2 
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
 
     //fetch the data
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res => res.json())
     .then(data => displayVideos(data.videos))
     .catch(error => console.log(error))
@@ -200,6 +200,11 @@ categories.forEach( (item) => {
 }
 
 
+
+document.getElementById('search-input').addEventListener('keyup', (e) =>{
+loadVideos(e.target.value);
+
+})
 
 
 loadCatagories()
